824bfae0404d0c33b5d212d98d14b89afbf291e7
Livia-Peng <livia_peng@163.com>
2024-12-14 12:17:28 +0800

    PC 端导航布局与样式

---
+++ src/views/layout/components/header/header-search/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="w-full guide-search">
+    <m-search
+      v-model="inputValue"
+      @search="onSearchHandler"
+      @clear="onSearchHandler">
+      <template #dropdown>
+        <div>dropdown</div>
+      </template>
+    </m-search>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  import { useStore } from 'vuex'
+  // import hintVue from './hint.vue'
+  // import historyVue from './history.vue'
+  // import themeVue from './theme.vue'
+
+  const inputValue = ref('')
+
+  // 搜索的回调事件
+  const onSearchHandler = (val) => {
+    console.log('触发 searchText 变化')
+    inputValue.value = val
+    if (val) {
+    }
+  }
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>这是首页内容</div>
+  <div class="h-screen">
+    <!-- 需要在 tailwind 中定义 h-header、h-main 高度 -->
+    <header-vue class="h-header"></header-vue>
+    <div class="h-main">
+      <main-vue></main-vue>
+    </div>
+    <floating-vue></floating-vue>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+  import headerVue from './components/header/index.vue'
+  import mainVue from './components/main.vue'
+  import floatingVue from './components/floating/index.vue'
+</script>
