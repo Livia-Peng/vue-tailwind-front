89502b1aff87e154a89c49f53327dff22f5fb1f1
Livia-Peng <livia_peng@163.com>
2024-12-17 01:20:52 +0800

    图片详情页，路由过渡动画

--- src/views/main/components/list/index.vue
+++ src/views/main/components/list/index.vue
@@ -7,19 +7,26 @@
         :picturePreReading="false"
         class="w-full px-1.5">
         <template v-slot="{ item, width }">
-          <ItemVue :data="item" :width="width"></ItemVue>
+          <ItemVue :data="item" :width="width" @click="onToPins"></ItemVue>
         </template>
       </m-waterfall>
     </m-infinite-list>
+    <!-- 大图详情处理 -->
+    <transition :css="false" @before-enter="beforeEnter" @enter="enter" @leave="leave">
+      <pins-vue v-if="isVisiblePins" :id="currentPins.id" />
+    </transition>
   </div>
 </template>
 
 <script setup>
   import { ref, watch } from 'vue'
+  import { useStore } from 'vuex'
+  import gsap from 'gsap'
   import { isMobileTerminal } from '@/utils/flexible'
   import { getPexelsList } from '@/api/pexels'
+  import { useEventListener } from '@vueuse/core'
   import ItemVue from './item.vue'
-  import { useStore } from 'vuex'
+  import pinsVue from '@/views/pins/pins.vue'
 
   const store = useStore()
 
@@ -87,4 +94,61 @@
       })
     }
   )
+
+  const isVisiblePins = ref(false)
+  const currentPins = ref({})
+  const onToPins = (item) => {
+    // 修改浏览器的 url
+    history.pushState(null, null, `/pins/${item.id}`)
+    currentPins.value = item
+    isVisiblePins.value = true
+  }
+
+  /**
+   * 监听浏览器后退按钮事件
+   */
+  useEventListener(window, 'popstate', () => {
+    isVisiblePins.value = false
+  })
+
+  /**
+   * 进入动画开始前
+   */
+  const beforeEnter = (el) => {
+    gsap.set(el, {
+      scaleX: 0,
+      scaleY: 0,
+      transformOrigin: '0 0',
+      translateX: currentPins.value.localtion?.translateX,
+      translateY: currentPins.value.localtion?.translateY,
+      opacity: 0
+    })
+  }
+  /**
+   * 进入动画执行中
+   */
+  const enter = (el, done) => {
+    gsap.to(el, {
+      duration: 0.3,
+      scaleX: 1,
+      scaleY: 1,
+      opacity: 1,
+      translateX: 0,
+      translateY: 0,
+      onComplete: done
+    })
+  }
+  /**
+   * 离开动画执行中
+   */
+  const leave = (el, done) => {
+    gsap.to(el, {
+      duration: 0.3,
+      scaleX: 0,
+      scaleY: 0,
+      x: currentPins.value.localtion?.translateX,
+      y: currentPins.value.localtion?.translateY,
+      opacity: 0
+    })
+  }
 </script>
