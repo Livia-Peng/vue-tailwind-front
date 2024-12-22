0816da7f0b8f2e42ec9b03aea53352a444f1abca
Livia-Peng <livia_peng@163.com>
2024-12-22 15:48:17 +0800

    移动端：路由切换动画，页面缓存，滚动位置缓存

--- src/store/getters.js
+++ src/store/getters.js
@@ -13,5 +13,14 @@ export default {
   userInfo: (state) => state.user.userInfo,
   token: (state) => state.user.token,
 
-  themeType: (state) => state.theme.themeType
+  themeType: (state) => state.theme.themeType,
+
+  // 路由跳转方式
+  routerType: (state) => {
+    // 在 PC 端下，永远为 none
+    if (!isMobileTerminal.value) {
+      return 'none'
+    }
+    return state.app.routerType
+  }
 }
